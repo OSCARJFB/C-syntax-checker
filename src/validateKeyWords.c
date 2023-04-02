f441587ee03d6a60ba5d38a1e0a8a9c780202488
OSCARJFB <mrsnask@hotmail.com>
2023-04-02 16:04:51 +0200

    Ascii enum
    Add the sum of each char in some of the keywords using ascii

--- src/validateKeyWords.c
+++ src/validateKeyWords.c
@@ -5,35 +5,9 @@
 
 #include "validateKeyWords.h"
 #include "main.h"
+#include <stdlib.h>
 
 int validateKeyWords(char buffer[BUFFER_SIZE], char err_msg[MSG_SIZE])
 {
-    // Native Data types
-    const char *k_void = "void", *k_char = "char", *k_short = "short", *k_int = "int", *k_float = "float", *k_long = "long", *k_double = "double";
-
-    // Properties. 
-    const char *k_register = "register", *k_extern = "extern", *k_static = "static", *k_auto = "auto", *k_const = "const", *k_unsigned = "unsigned", *k_signed = "unsigned";;
-
-    // Control flow statements
-    const char *k_if = "if", *k_else = "else", *k_switch = "switch", *k_case = "case", *k_default = "default", k_break = "break"; 
-
-    // Loops 
-    const char *k_do = "do", *k_while = "while", *k_for = "for", *k_continue = "continue";
-
-    // return 
-    const char *k_return = "return"; 
-
-    // sizeof
-    const char *k_sizeof = "sizeof";
-    
-    // Labels
-    const char *k_goto = "goto";
-
-    // typedef
-    const char k_tDef = "typedef";
-
-    // Struct and Union
-    const char *k_struct = "struct", *k_union = "union"; 
-
     return 0;
 }
--- src/validateKeyWords.h
+++ src/validateKeyWords.h
@@ -8,6 +8,32 @@
 
 #include "main.h"
 
+enum storageClasses
+{
+    kw_register = 869,   // Can't be used with functions.    
+    kw_extern = 662,         
+    kw_static = 648,         
+    kw_auto = 441,           
+};
+
+enum properties
+{
+    kw_const = 551,        
+    kw_signed = 634,         
+    kw_unsigned = 861,             
+};
+
+enum dataTypes
+{
+    kw_void = 434,        
+    kw_char = 414,         
+    kw_int = 331,            
+    kw_float = 534,  
+    kw_long = 432,
+    kw_double = 653,             
+};
+
 int validateKeyWords(char buffer[BUFFER_SIZE], char err_msg[MSG_SIZE]);
+inline int getKeyWordValue(const char *keyWord);
 
 #endif // VALIDATEKEYWORDS_H
